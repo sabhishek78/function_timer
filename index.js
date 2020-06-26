2d28b52ad10fbb6a6fabf119c02f08a89ee596a2
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-06-26 11:01:40 +0530

    code corrected

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 function functionTimer(logs){
     let logsArray=logs.map((e)=>e.split(':'));
+    for(let i=0;i<logsArray.length;i++){
+        logsArray[i][1]==='start'?logsArray[i][2]=parseInt(logsArray[i][2]):logsArray[i][2]=parseInt(logsArray[i][2])+1;
+        logsArray[i][0]=parseInt(logsArray[i][0]);
+    }
     console.log(logsArray);
     let functionStack=[];
-    functionStack.push(parseInt(logsArray[0][0]));
-    let timeStamp=0;
-    let previousTag="start";
+    let timeStamp;
     let timeArray=new Array(logsArray.length).fill(0);
+    functionStack.push(logsArray[0][0]);
+    timeArray[0]=logsArray[0][2];
+    timeStamp=logsArray[0][2];
     for(let i=1;i<logsArray.length;i++){
         if(logsArray[i][1]==='start'){
-
-           functionStack.push(logsArray[i][0]);
-           timeArray[functionStack[i-1]]+=parseInt(logsArray[i][2])-timeStamp;
-           timeStamp=parseInt(logsArray[i][2]);
-           previousTag="start";
+            timeStamp=logsArray[i][2];
+            functionStack.push(logsArray[i][0]);
+            timeArray[functionStack[i-1]]+=logsArray[i][2];
+            timeStamp=logsArray[i][2];
         }
         if(logsArray[i][1]==='end'){
-            let task=functionStack.pop();
-            if(previousTag==='start'){
-                timeArray[task]+=parseInt(logsArray[i][2])-timeStamp+1;
-            }
-            if(previousTag==='end'){
-                timeArray[task]+=parseInt(logsArray[i][2])-timeStamp;
-            }
+            let functionId=functionStack.pop();
+            timeArray[functionId]+=logsArray[i][2]-timeStamp;
             timeStamp=logsArray[i][2];
-            previousTag="end";
+
         }
     }
     return timeArray.filter((e)=>e!==0);
